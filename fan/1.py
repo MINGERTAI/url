6dc7850a1fe26ada50337d21e6fa6ce2fe9ac5b7
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-14 16:42:27 +0800

    Update 1.py

--- fan/1.py
+++ fan/1.py
@@ -3,36 +3,51 @@ import json
 import hashlib
 import configparser
 import re
-import os
 
 headers = {'User-Agent': 'okhttp/3.15'}
 
 def save_website_content_as_json_and_check_updates(url, file_name):
     config = configparser.ConfigParser()
-    config.read(os.path.join("fan", "FatCat", "config.ini"))
+    config.read("config.ini")
     
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            data = response.json()
-
+            data = response.json()  # 假设响应内容是JSON格式
+            
+            # 计算返回数据的md5值来检查数据是否有更新
             new_md5 = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
             old_md5 = config.get('DEFAULT', 'md5', fallback='')
             if new_md5 != old_md5:
                 print("检测到更新。")
+                # 更新配置文件中的md5值
                 config['DEFAULT']['md5'] = new_md5
-                with open(os.path.join("fan", "FatCat", "config.ini"), 'w') as configfile:
+                with open('config.ini', 'w') as configfile:
                     config.write(configfile)
-
-                # 检查并修改spider字段
-                if 'spider' in data:
-                    original_url = data['spider'].split(';md5;')[0]
-                    data['spider'] = './fan/JAR/fan.txt;md5;5ee96d541532306c3acc8c0fc229acbf'
                 
-                # 保存修改后的JSON数据
+                # 将响应内容保存为JSON文件
                 with open(file_name + '.json', 'w', encoding='utf-8') as file:
                     json.dump(data, file, indent=4, ensure_ascii=False)
                 print(f"数据已以JSON格式保存到{file_name}.json")
+                
+                # 从JSON数据中提取包含jar文件URL和md5值的"spider"字段
+                spider = data.get('spider')
+                if spider:
+                    jar_url, jar_md5 = re.match(r'http://[^/]+/jar/(.+?);md5;([a-f0-9]{32})', spider).groups()
+                    full_jar_url = f"http://like.xn--z7x900a.com/jar/{jar_url}"
+                    # 下载jar文件
+                    jar_response = requests.get(full_jar_url)
+                    if jar_response.status_code == 200:
+                        jar_file_name = jar_url.split('/')[-1]  # 从URL提取文件名
+                        with open(jar_file_name, 'wb') as jar_file:
+                            jar_file.write(jar_response.content)
+                        print(f"jar文件已下载到：{jar_file_name}")
+                        config['DEFAULT']['jar_md5'] = jar_md5
+                        with open('config.ini', 'w') as configfile:
+                            config.write(configfile)
+                        print("jar文件的md5值已更新。")
+                    else:
+                        print(f"jar文件下载失败，状态码：{jar_response.status_code}")
             else:
                 print("未检测到更新。")
         else:
@@ -40,32 +55,9 @@ def save_website_content_as_json_and_check_updates(url, file_name):
     except Exception as e:
         print(f"发生错误：{str(e)}")
 
+# 目标URL
 url = 'http://肥猫.com'
+# 文件名，不包括扩展名
 file_name = 'website_content'
+
 save_website_content_as_json_and_check_updates(url, file_name)
-
-def diy_conf(content):
-    # 这里添加您需要的任何特定修改
-    modified_content = content
-    return modified_content
-
-# 读取保存的JSON数据
-with open(file_name + '.json', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# 修改内容
-modified_content = diy_conf(content)
-
-# 将修改后的内容写入C.json
-with open('C.json', 'w', encoding='utf-8') as f:
-    f.write(modified_content)
-import json
-
-
-# 将数据保存到文件，不使用缩进
-with open('data.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False)
-
-# 或者，如果您想要得到字符串形式的 JSON 数据，可以使用 json.dumps()
-json_string = json.dumps(data, ensure_ascii=False)
-print(json_string)
