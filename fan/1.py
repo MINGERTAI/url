0b85f90d047997c348abbaa62a788819d0fd18f6
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-14 11:17:49 +0800

    Update 1.py

--- fan/1.py
+++ fan/1.py
@@ -11,35 +11,30 @@ def save_website_content_as_json_and_check_updates(url, file_name):
     config = configparser.ConfigParser()
     config.read(os.path.join("fan", "FatCat", "config.ini"))  # 使用os.path.join确保路径正确
     
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            content = response.text  # 获取响应的文本内容
-            data = response.json()  # 假设响应内容是JSON格式
-            
-            # 计算返回数据的md5值来检查数据是否有更新
-            new_md5 = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
-            old_md5 = config.get('DEFAULT', 'md5', fallback='')
-            if new_md5 != old_md5:
-                print("检测到更新。")
-                # 更新配置文件中的md5值
-                config['DEFAULT']['md5'] = new_md5
-                with open(os.path.join("fan", "FatCat", "config.ini"), 'w') as configfile:
-                    config.write(configfile)
+try:
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()  # 假设响应内容是JSON格式
 
-                # 注意：以下代码假设content变量是一个包含特定格式的字符串
-                # 如果您的意图是修改data变量，请确保您的逻辑与数据结构相匹配
-                url_search_result = re.search(r'spider"\:"(.*);md5;', content)
-                if url_search_result:
-                    url = url_search_result.group(1)
-                    content = content.replace(url, './fan/FatCat/PandaQ240609.jar')
-                else:
-                    print("未能在响应内容中找到预期的URL格式。")
-                
-                # 将响应内容保存为JSON文件
-                with open(file_name + '.json', 'w', encoding='utf-8') as file:
-                    json.dump(data, file, indent=4, ensure_ascii=False)
-                print(f"数据已以JSON格式保存到{file_name}.json")
+        # 计算返回数据的md5值来检查数据是否有更新
+        new_md5 = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
+        old_md5 = config.get('DEFAULT', 'md5', fallback='')
+        if new_md5 != old_md5:
+            print("检测到更新。")
+            # 更新配置文件中的md5值
+            config['DEFAULT']['md5'] = new_md5
+            with open(os.path.join("fan", "FatCat", "config.ini"), 'w') as configfile:
+                config.write(configfile)
+
+            # 直接在data字典上修改spider字段的值
+            if 'spider' in data:
+                original_url = data['spider'].split(';md5;')[0]  # 假设spider字段的格式为"URL;md5;MD5值"
+                data['spider'] = data['spider'].replace(original_url, './fan/FatCat/PandaQ240609.jar')
+
+            # 将修改后的data保存为JSON文件
+            with open(file_name + '.json', 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+            print(f"数据已以JSON格式保存到{file_name}.json")
                 
                 # 从JSON数据中提取包含jar文件URL和md5值的"spider"字段
                 spider = data.get('spider')
