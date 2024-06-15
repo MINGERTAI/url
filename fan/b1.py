900fb3214231c499e3e5390d6e0c6f8aafc642a4
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-15 11:26:07 +0800

    Update b1.py

--- fan/b1.py
+++ fan/b1.py
@@ -70,7 +70,7 @@ def save_website_content_as_json_and_check_updates(url, file_name):
                     json.dump(data, file, indent=4, ensure_ascii=False)
                 print(f"数据已以JSON格式保存到{file_name}")
                 with open('11.json', 'w', newline='', encoding='utf-8') as file:
-                    json.dump(data, file, indent=4, ensure_ascii=False)
+                    json.dump(data, ensure_ascii=False)
             else:
                 print("未检测到更新。")
         else:
