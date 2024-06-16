7c68d20c92cf0ff86ba91515854e4742149f26ce
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-16 14:39:02 +0800

    Update FatCat.py

--- fan/FatCat.py
+++ fan/FatCat.py
@@ -65,8 +65,9 @@ def save_website_content_as_json_and_check_updates(url, file_name):
                     json.dump(data, file, indent=4, ensure_ascii=False)
                 print(f"数据已以JSON格式保存到{file_name}")
 
+                sites = data.get('sites')
                 with open('1.json', 'w', newline='', encoding='utf-8') as f:
-                    f.write(data)
+                    f.write(sites)
                 
             else:
                 print("未检测到更新。")
