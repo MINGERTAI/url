05b7672db78941db1aec45bc2b9bc4df4910a074
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-16 10:29:04 +0800

    Update main.py

--- fan/main.py
+++ fan/main.py
@@ -80,54 +80,5 @@ def check_and_update():
     except Exception as ex:
         LocalFile.write_LogFile('Main-Line-108-Exception:' + str(ex))
 
-def load_file(filepath):
-    """加载本地文件内容"""
-    if os.path.exists(filepath):
-        return LocalFile.read_LocalFile(filepath).replace('\r', '').replace('\n\n', '\n')
-    return None
-
-def validate_line(j, r_sites_err):
-    """验证行内容是否有效"""
-    return j and '"key":' in j and '"name":' in j and '"type":' in j and r_sites_err.find(j) == -1
-
-def clean_line(j):
-    """清理行内容"""
-    j = j.strip(',')
-    if len(j.split('}')) > len(j.split('{')):
-        j = j.strip(',')[:-1].strip(',')
-    return j
-
-def validate_url(url):
-    """验证 URL 是否有效"""
-    if url.startswith('http'):
-        ustat = NetFile.url_stat(url, 60, 60)
-        return ustat not in [404, 0]
-    return True
-
-def is_duplicate(tv, addtv, nsfw, spare, r_sites_err):
-    """检查是否存在重复内容"""
-    j = json.dumps(tv)
-    return (addtv + spare + nsfw).find(j) > -1 or (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1
-
-def log_error(r_sites_err, j):
-    """记录错误日志"""
-    r_sites_err += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(404) + ':' + j + ','
-
-def unique_key(tv, addtv, nsfw, r_sites_err):
-    """检查是否存在唯一键"""
-    return (addtv + nsfw + r_sites_err).find('"api":"' + tv['api'] + '"') > -1
-
-def validate_api(tv, addtv, nsfw, r_sites_err):
-    """验证 API 是否有效"""
-    api = tv['api']
-    if (addtv + nsfw + r_sites_err).find(api) > -1:
-        return False
-    if api.startswith('http'):
-        ustat = NetFile.url_stat(api, 60, 60)
-        if ustat in [404, 0]:
-            log_error(r_sites_err, json.dumps(tv))
-            return False
-    return True
-
 if __name__ == "__main__":
     main()
