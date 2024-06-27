6da0dbe0181d1ab7b35a129a1bd5c4a0086f94bd
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-06-27 17:03:02 +0800

    Update new.py

--- fan/new.py
+++ fan/new.py
@@ -75,23 +75,14 @@ def modify_content(content):   # 更改自定义
     # 删除 //{"key":  整行
     content = re.sub(r'^\s*//\{"key":.*\n', '', content, flags=re.MULTILINE)
 
-    # 删除 "lives":[ 整行
-    content = re.sub(r'^\s*"lives":[.*\n', '', content, flags=re.MULTILINE)
-    # 删除 {"name":"live","type": 整行
-    content = re.sub(r'^\s*{"name":"live","type":.*\n', '', content, flags=re.MULTILINE)
-    # 删除 { "name": "XIUTAN", "ua": 整行
-    content = re.sub(r'^\s*{ "name": "XIUTAN", "ua":.*\n', '', content, flags=re.MULTILINE)
-    # 删除 "logo":"https:\/\/fs-im-kefu.7moor-fs1.com 整行
-    content = re.sub(r'^\s*"logo":"https:\/\/fs-im-kefu.7moor-fs1.com.*\n', '', content, flags=re.MULTILINE)
-
     # 替换"logo"URL
-    #new_logo_url = "https://ghproxy.net/https://raw.githubusercontent.com/ne7359/url/main/fan/AW1.gif"
-    #content = re.sub(r'"logo":"[^"]+"', f'"logo":"{new_logo_url}"', content)
+    new_logo_url = "https://ghproxy.net/https://raw.githubusercontent.com/ne7359/url/main/fan/AW1.gif"
+    content = re.sub(r'"logo":"[^"]+"', f'"logo":"{new_logo_url}"', content)
 
     # 替换"live"URL
-    #original_url = "https://www.huichunniao.cn/xh/lib/live.txt"
-    #replacement_url = "https://ghproxy.net/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
-    #content = content.replace(original_url, replacement_url)
+    original_url = "https://www.huichunniao.cn/xh/lib/live.txt"
+    replacement_url = "https://ghproxy.net/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+    content = content.replace(original_url, replacement_url)
     return content
     
 def diy_conf(content):
