e9ba828ca316b57db9e6a9dab9d25313ddd1d9af
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-01 20:14:50 +0800

    Update dianshi.py

--- TVUrl-check/py/dianshi.py
+++ TVUrl-check/py/dianshi.py
@@ -99,10 +99,10 @@ if menu == 'check':
 
         LocalFile.write_LocalFile('./out/r_sites_err.txt', r_sites_err.strip('\r\n'))
         print('Line-96:/res/r_sites_err.txt已更新。')
-        content = addtv + '\r\n' + nsfw + '\r\n' + spare
         
         # 应用删除特定行的逻辑
         content = remove_line(content)
+        content = addtv + '\r\n' + nsfw + '\r\n' + spare
         LocalFile.write_LocalFile('./out/dianshi.txt', content)
     except Exception as ex:
         LocalFile.write_LogFile('Main-Line-108-Exception:' + str(ex))
