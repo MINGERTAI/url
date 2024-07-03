7dd601769a08dfedb57e5c67b6d6f6be2b50cb93
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-03 11:23:20 +0800

    Update ba.py

--- TVUrl-check/py/ba.py
+++ TVUrl-check/py/ba.py
@@ -1,11 +1,9 @@
 import datetime
-from gettext import find
 import json
 import os
-import requests
+import re
 import sys
-from cls import LocalFile
-from cls import NetFile
+from cls import LocalFile, NetFile
 
 # 获取传递的参数
 try:
@@ -14,101 +12,112 @@ try:
     if(len(sys.argv[1:]) > 1):
         cid = sys.argv[1:][1]
 except:
-    menu = 'check'
+    menu = 'init'
 print('menu: ' + menu)
 
 # 下载Node.json中的所有Url订阅链接将其合并，生成本地vpei-new.txt，同步至Github后改名为vpei.txt文件
-if(menu == 'check'):
+if menu == 'uptvbox':
     try:
-        if(os.path.exists('./out/0821.txt')):
-            tvbox = LocalFile.read_LocalFile('./out/dianshi.txt').replace('\r','').replace('\n\n','\n')
+        if os.path.exists('./out/tmp.txt'):
+            tvbox = LocalFile.read_LocalFile('./out/tmp.txt').replace('\r', '').replace('\n\n', '\n')
         else:
-            tvbox = LocalFile.read_LocalFile('./code/dianshi.json').replace('\r','').replace('\n\n','\n')
-        r_sites_err = LocalFile.read_LocalFile("./code/r_sites_err.txt")
+            tvbox = LocalFile.read_LocalFile('./code/dianshi.json').replace('\r', '').replace('\n\n', '\n')
+        addjson = LocalFile.read_LocalFile("./code/addjson.txt")
 
         addtv = ''
         nsfw = ''
         spare = ''
-        tvbox = tvbox.replace('//{','\n{')
+        tvbox = tvbox.replace('//{', '\n{')
         for j in tvbox.split('\n'):
             try:
-                if(j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 and r_sites_err.find(j) == -1):
+                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 and addjson.find(j) == -1:
                     j = j.strip(',')
-                    if(len(j.split('}')) > len(j.split('{'))):
+                    if len(j.split('}')) > len(j.split('{')):
                         j = j.strip(',')[:-1].strip(',')
                     tv = json.loads(j)
                     # 检查自定义Jar文件是否存在
-                    if('jar' in tv.keys()):
+                    if 'jar' in tv.keys():
                         jar = tv['jar']
-                        if(jar.find('http') == 0):
+                        if jar.find('http') == 0:
                             ustat = NetFile.url_stat(jar, 60, 60)
-                            if(ustat == 404 or ustat == 0):
-                                j = j.replace(',"jar":"' + jar + '"', '')                         
+                            if ustat == 404 or ustat == 0:
+                                j = j.replace(',"jar":"' + jar + '"', '')
                     # 过滤重复的电影网站
-                    if((addtv + spare + nsfw).find(j) > -1):
+                    if (addtv + spare + nsfw).find(j) > -1:
                         continue
                     # 过滤重复Key的电影网站
-                    if((addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1):
+                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
                         spare += '\r\n' + j + ','
                         continue
                     # 分类去重
                     id = tv['type']
-                    if(id == 3):
-                        if('ext' in tv.keys()):
+                    if id == 3:
+                        if 'ext' in tv.keys():
                             ext = tv['ext']
-                            if((addtv + nsfw + r_sites_err).find(ext) > -1):
+                            if (addtv + nsfw + addjson).find(ext) > -1:
                                 continue
                             else:
-                                if(ext.find('http') == 0):
+                                if ext.find('http') == 0:
                                     ustat = NetFile.url_stat(ext, 60, 60)
-                                    if(ustat == 404 or ustat == 0):
-                                        r_sites_err += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
+                                    if ustat == 404 or ustat == 0:
+                                        addjson += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
                                         continue
-                        else:
-                            # 未配置Ext信息，让api值唯一
-                            if((addtv + nsfw + r_sites_err).find('"api":"' + tv['api'] + '"') > -1):
-                                continue
-                        
-                    elif(id >= 0):
+                    elif id >= 0:
                         api = tv['api']
-                        if((addtv + nsfw + r_sites_err).find(api) > -1):
+                        if (addtv + nsfw + addjson).find(api) > -1:
                             continue
                         else:
-                            if(api.find('http') == 0):
+                            if api.find('http') == 0:
                                 ustat = NetFile.url_stat(api, 60, 60)
-                                if(ustat == 404 or ustat == 0):
-                                    r_sites_err += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
+                                if ustat == 404 or ustat == 0:
+                                    addjson += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
                                     continue
-
                     else:
                         spare += '\r\n' + j + ','
                     
-                    if(tv['name'].find('*') > -1):
+                    if tv['name'].find('*') > -1:
                         nsfw += '\r\n' + j + ','
-                    elif(j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1):
+                    elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
                         addtv += '\r\n' + j + ','
                 else:
                     print('Main-Line-91-not-tvsite-url:' + j)
-
             except Exception as ex:
                 LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
-        LocalFile.write_LocalFile('./code/r_sites_err.txt', r_sites_err.strip('\r\n'))
-        print('Line-96:/res/r_sites_err.txt已更新。')
-        
-        # 将修改后的内容组合
-        final_content = addtv + '\r\n' + nsfw + '\r\n' + spare
-        
-        # 将修改后的内容写回文件
-        LocalFile.write_LocalFile('./out/dianshi.txt', final_content)
-        
+        myjson = addtv + '\r\n' + nsfw + '\r\n' + spare
+        LocalFile.write_LocalFile('./out/tmp.txt', myjson)
+        print('Line-96:./out/tmp.txt已更新。')
+
     except Exception as ex:
         LocalFile.write_LogFile('Main-Line-108-Exception:' + str(ex))
-        
-        # 删除指定行
-        #tvbox = delete_lines(content)
-        
-        # 将修改后的内容写回文件
-        #LocalFile.write_LocalFile('./out/dianshi.txt', addtv + '\r\n' + nsfw + '\r\n' + spare)
+
+if menu == 'del':
+    def remove_line(content):
+        patterns = [
+            r'{"key":"drpy_js_豆瓣"(.|\n)*(?={"key":"高中教育")',   # 删除自{"key":"drpy_js_豆瓣"行到{"key":"高中教育"前一行所有
+            r'^\s*{"key":"高中教育".*\n',
+            r'^\s*{"key":"蛋蛋".*'      # 删除最后一行不可以添加\n换行代码
+        ]    
+        for pattern in patterns:
+            content = re.sub(pattern, '', content, flags=re.MULTILINE)
+        return content
+    # 加载内容
+    content = LocalFile.read_LocalFile('./out/tmp.txt').replace('\r', '').replace('\n\n', '\n')
+    # 应用删除特定行的逻辑
+    content = remove_line(content)
+    LocalFile.write_LocalFile('./out/json.txt', content)
+
+######
+
+    #try:
+        #if os.path.exists('./out/tmp.txt'):
+            #content = LocalFile.read_LocalFile('./out/tmp.txt').replace('\r', '').replace('\n\n', '\n')
+        #else:
+            #content = LocalFile.read_LocalFile('./out/json.txt').replace('\r', '').replace('\n\n', '\n')
+
+        # 应用删除特定行的逻辑
+        #content = remove_line(content)
+        #LocalFile.write_LocalFile('./out/newjson.txt', content)
+
     #except Exception as ex:
         #LocalFile.write_LogFile('Main-Line-108-Exception:' + str(ex))
