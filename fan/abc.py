bed7d6f1c95e275fbd58b80298f89e057b2faf36
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-04 18:13:36 +0800

    Update abc.py

--- fan/abc.py
+++ fan/abc.py
@@ -53,8 +53,19 @@ if menu == 'tvbox':
                     if len(j.split('}')) > len(j.split('{')):
                         j = j.strip(',')[:-1].strip(',')
                     tv = json.loads(j)
+                    # 过滤重复的电影网站
                     if (addtv + spare + nsfw).find(j) > -1:
                         continue
+                    # 过滤重复Key的电影网站
+                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
+                        spare += '\r\n' + j + ','
+                        continue
+                    else:
+                        spare += '\r\n' + j + ','                
+                    if tv['name'].find('*') > -1:
+                        nsfw += '\r\n' + j + ','
+                    elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
+                        addtv += '\r\n' + j + ','
             except Exception as ex:
                 LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
