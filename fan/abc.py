50a25599248ebd5fd5d7a2cd74ddb261e1047a92
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-04 18:16:08 +0800

    Update abc.py

--- fan/abc.py
+++ fan/abc.py
@@ -70,8 +70,6 @@ if menu == 'tvbox':
                 LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
         content = addtv + '\r\n' + nsfw + '\r\n' + spare
-        LocalFile.write_LocalFile('./out/pull.txt', content)
-        print('Line-96:./out/pull.txt已更新。')
         content = remove_line(content)
         LocalFile.write_LocalFile('./out/json.txt', content)
         print('Line-96:./out/json.txt已更新。')
