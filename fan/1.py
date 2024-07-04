f858f7ee05923f65e12ab14860e1ea6765b5b480
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-04 22:17:29 +0800

    Update 1.py

--- fan/1.py
+++ fan/1.py
@@ -27,61 +27,73 @@ resurl = 'https://github.com/qist/tvbox/blob/master/'
 
 #对程序的基本信息进行下载更新，下载IPFS网关信息和过滤列表信息
 if(menu == 'init'):
-    filename = 'js.json|dianshi.json'
-    for i in filename.split('|'):
-        try:
-            File = NetFile.url_to_str(resurl + '' + i, 240, 240)
-            if(len(File) > 10240):
-                LocalFile.write_LocalFile('./out/' + i, File.strip('\n')) 
-                print('Get-File-is-True:' + resurl + '' + i + ' FileSize:' + str(len(File)))
-        except Exception as ex:
-            print('Get-File-is-False:' + resurl + '' + i + '\n' + str(ex))
-
-def write_json():
-    '''
-    # 写入/追加json文件
-    :param obj:
-    :return:
-    '''
-    post=set()
-    fp=[]
-    for p in range(len(json)):
-            if json[p]['text'] not in post:  #如果set中没有这个值（set特性是不能重复）
-                    fp.append(json[p])  #fp数组将这个不重复的值存进
-                    post.add(json[p]['text'])  #set存进这个不重复的值，为了后面判断是否重复做准备
-                    print(json[p])
-                    print(post)
-                    print(json[p]['text'])
-    print(fp)
-
-    #首先读取已有的json文件中的内容
-    post=set()
-    item_list = []
-    alltypename = ''
-    with open('./out/tvlist.json', 'r', encoding='utf-8') as f:
-        load_dict = json.load(f)
-        num_item = len(load_dict)
-        for i in range(num_item):
+    tvbox = 'js.json|dianshi.json'
+        addtv = ''
+        nsfw = ''
+        spare = ''
+        tvbox = tvbox.replace('//{', '\n{')
+        for j in tvbox.split('\n'):
             try:
-                if load_dict[i]['tvmd5'] not in post:   #如果set中没有这个值（set特性是不能重复）
-                    post.add(str(load_dict[i]['tvmd5']))  #set存进这个不重复的值，为了后面判断是否重复做准备
-                    typename = load_dict[i]['typename']
-                    if(alltypename.find(typename) == -1):
-                        alltypename = alltypename + '\r\n' + typename
-                    tvname = load_dict[i]['tvname']
-                    tvmd5 = load_dict[i]['tvmd5']
-                    tvurl = load_dict[i]['tvurl']
-
-                    item_dict = {'typename':typename, 'tvname':tvname,'tvmd5':tvmd5, 'tvurl':tvurl}
-                    item_list.append(item_dict)
-                    item_list.append(load_dict[i])
+                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 and addjson.find(j) == -1:
+                    j = j.strip(',')
+                    if len(j.split('}')) > len(j.split('{')):
+                        j = j.strip(',')[:-1].strip(',')
+                    tv = json.loads(j)
+                    # 检查自定义Jar文件是否存在
+                    if 'jar' in tv.keys():
+                        jar = tv['jar']
+                        if jar.find('http') == 0:
+                            ustat = NetFile.url_stat(jar, 60, 60)
+                            if ustat == 404 or ustat == 0:
+                                j = j.replace(',"jar":"' + jar + '"', '')
+                    # 过滤重复的电影网站
+                    if (addtv + spare + nsfw).find(j) > -1:
+                        continue
+                    # 过滤重复Key的电影网站
+                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
+                        spare += '\r\n' + j + ','
+                        continue
+                    # 分类去重
+                    id = tv['type']
+                    if id == 3:
+                        if 'ext' in tv.keys():
+                            ext = tv['ext']
+                            if (addtv + nsfw + addjson).find(ext) > -1:
+                                continue
+                            else:
+                                if ext.find('http') == 0:
+                                    ustat = NetFile.url_stat(ext, 60, 60)
+                                    if ustat == 404 or ustat == 0:
+                                        addjson += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
+                                        continue
+                    elif id >= 0:
+                        api = tv['api']
+                        if (addtv + nsfw + addjson).find(api) > -1:
+                            continue
+                        else:
+                            if api.find('http') == 0:
+                                ustat = NetFile.url_stat(api, 60, 60)
+                                if ustat == 404 or ustat == 0:
+                                    addjson += '\r\n[' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] ' + str(ustat) + ':' + j + ','
+                                    continue
+                    else:
+                        spare += '\r\n' + j + ','
+                    
+                    if tv['name'].find('*') > -1:
+                        nsfw += '\r\n' + j + ','
+                    elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
+                        addtv += '\r\n' + j + ','
+                else:
+                    print('Main-Line-91-not-tvsite-url:' + j)
             except Exception as ex:
-                LocalFile.write_LogFile('Main-Line-87-Exception:' + str(ex))
-            
-    #读取已有内容完毕
-    #将新传入的dict对象追加至list中
-    item_list.append(obj)
-    #将追加的内容与原有内容写回（覆盖）原文件
-    with open('./out/tvlist.json', 'w', encoding='utf-8') as f2:
-        json.dump(item_list, f2, ensure_ascii=False)
-    LocalFile.write_LocalFile('./out/typename.txt', alltypename.lstrip('\r\n'))
+                LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
+        
+        content = addtv + '\r\n' + nsfw + '\r\n' + spare
+        LocalFile.write_LocalFile('./out/123.txt', content)
+        print('Line-96:./out/pull.txt已更新。')
+        #content = remove_line(content)
+        #LocalFile.write_LocalFile('./out/json.txt', content)
+        #print('Line-96:./out/json.txt已更新。')
+
+    except Exception as ex:
+        LocalFile.write_LogFile('Main-Line-108-Exception:' + str(ex))
