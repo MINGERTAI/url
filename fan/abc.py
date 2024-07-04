10fe5fb79fe201d0d6a8e34154d5f8b6e2020812
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-04 17:57:56 +0800

    Update abc.py

--- fan/abc.py
+++ fan/abc.py
@@ -46,28 +46,6 @@ if menu == 'tvbox':
         nsfw = ''
         spare = ''
         tvbox = tvbox.replace('//{', '\n{')
-        for j in tvbox.split('\n'):
-            try:
-                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 == -1:
-                    j = j.strip(',')
-                    if len(j.split('}')) > len(j.split('{')):
-                        j = j.strip(',')[:-1].strip(',')
-                    tv = json.loads(j)
-                    # 过滤重复的电影网站
-                    if (addtv + spare + nsfw).find(j) > -1:
-                        continue
-                    # 过滤重复Key的电影网站
-                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
-                        spare += '\r\n' + j + ','
-                        continue
-                    else:
-                        spare += '\r\n' + j + ','                
-                    if tv['name'].find('*') > -1:
-                        nsfw += '\r\n' + j + ','
-                    elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
-                        addtv += '\r\n' + j + ','
-            except Exception as ex:
-                LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
         content = addtv + '\r\n' + nsfw + '\r\n' + spare
         LocalFile.write_LocalFile('./out/pull.txt', content)
