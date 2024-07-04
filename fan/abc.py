c3eee0e3688c0eb39e07df5b1932705d87c49c8d
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-04 17:46:46 +0800

    Update abc.py

--- fan/abc.py
+++ fan/abc.py
@@ -60,10 +60,8 @@ if menu == 'tvbox':
                     if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
                         spare += '\r\n' + j + ','
                         continue
-
                     else:
-                        spare += '\r\n' + j + ','
-                    
+                        spare += '\r\n' + j + ','                
                     if tv['name'].find('*') > -1:
                         nsfw += '\r\n' + j + ','
                     elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
