9f5ac448c343a70059d5b8ab0b87a90d9b4886c3
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-05 17:59:16 +0800

    Update ba_fatcat.py

--- fan/ba_fatcat.py
+++ fan/ba_fatcat.py
@@ -25,13 +25,10 @@ def save_website_content_as_json_and_check_updates(url, file_name):
                 with open('config.ini', 'w') as configfile:
                     config.write(configfile)
 
-                data_list =  json.dump(data, file, indent=4, ensure_ascii=False)
+                
                 # 查找并删除包含指定键值对的字典项
                 key_to_remove = "drpy_js_豆瓣"
-                data_list = [item for item in data_list if item.get('key') != key_to_remove]
-                # 将结果转换回JSON格式
-                data = json.dumps(data_list, ensure_ascii=False, indent=4)
-                print(data)
+                data = [item for item in data_list if item.get('key') != key_to_remove]
                 
                 # 将响应内容保存为JSON文件
                 with open(file_name + '.json', 'w', encoding='utf-8') as file:
