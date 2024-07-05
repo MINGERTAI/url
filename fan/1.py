2d5dabdad1eed27ae2740a3ead46d0b1159f1e00
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-05 11:39:18 +0800

    Update 1.py

--- fan/1.py
+++ fan/1.py
@@ -6,14 +6,55 @@ import configparser
 headers = {'User-Agent': 'okhttp/3.15'}
 
 def get_fan_conf():
-    # config = configparser.ConfigParser()
-    # config.read("fan/config.ini")
+    config = configparser.ConfigParser()
+    config.read("fan/config.ini")
 
     url = 'https://github.com/ne7359/tvurl/blob/main/dianshi.json'
     response = requests.get(url, headers=headers)
+    match = re.search(r'[A-Za-z0]{8}\*\*(.*)', response.text)
+
+    if not match:
+        return
+    result = match.group(1)
+
+    m = hashlib.md5()
+    m.update(result.encode('utf-8'))
+    md5 = m.hexdigest()
+
+    try:
+        old_md5 = config.get("md5", "conf")
+        if md5 == old_md5:
+            print("No update needed")
+            return
+    except:
+        pass
+
+    content = base64.b64decode(result).decode('utf-8')
+    url = re.search(r'spider"\:"(.*);md5;', content).group(1)
+    content = content.replace(url, './out/fan.txt')
+ 
+
+    with open('./out/xo.json', 'w', newline='', encoding='utf-8') as f:
+        f.write(content)
+
+
+    # Update conf.md5
+    config.set("md5", "conf", md5)
+    with open("./out/config.ini", "w") as f:
+        config.write(f)
+
+    jmd5 = re.search(r';md5;(\w+)"', content).group(1)
+    current_md5 = config.get("md5", "jar").strip()
+
+    if jmd5 != current_md5:
+        # Update jar.md5
+        config.set("md5", "jar", jmd5)
+        with open("./out/config.ini", "w") as f:
+            config.write(f)
 
         response = requests.get(url)
         with open("./out/fan.txt", "wb") as f:
             f.write(response.content)
+
 if __name__ == '__main__':
     get_fan_conf()
