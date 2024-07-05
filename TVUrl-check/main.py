539823678c84a1ad92296fdcf679342f5c52d770
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-05 20:36:51 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -22,7 +22,6 @@ print('menu: ' + menu)
 # 下载Node.json中的所有Url订阅链接将其合并，生成本地vpei-new.txt，同步至Github后改名为vpei.txt文件
 if(menu == 'check'):
     try:
-    try:
         if os.path.exists('./code/dianshi.json'):
             tvbox = LocalFile.read_LocalFile('./FatCat.json').replace('\r', '').replace('\n\n', '\n')
         else:
