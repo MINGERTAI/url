3b88760e9bef5937068bbd435750c7f190de659e
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-05 11:33:27 +0800

    Update 1.py

--- fan/1.py
+++ fan/1.py
@@ -11,31 +11,12 @@ def get_fan_conf():
 
     url = 'https://github.com/ne7359/tvurl/blob/main/dianshi.json'
     response = requests.get(url, headers=headers)
-    match = re.search(response.text)
 
-    if not match:
-        return
-    result = match.group(1)
-
-    m = hashlib.md5()
-    m.update(result.encode('utf-8'))
-    md5 = m.hexdigest()
-
-    url = re.search(r'spider"\:"(.*);md5;', content).group(1)
     content = content.replace(url, './out/fan.txt')
 
     with open('out/11.json', 'w', newline='', encoding='utf-8') as f:
         f.write(content)
 
-    # Update conf.md5
-    config.set("md5", "conf", md5)
-
-    jmd5 = re.search(r';md5;(\w+)"', content).group(1)
-    current_md5 = config.get("md5", "jar").strip()
-
-    if jmd5 != current_md5:
-        # Update jar.md5
-        config.set("md5", "jar", jmd5)
 
         response = requests.get(url)
         with open("./out/fan.txt", "wb") as f:
