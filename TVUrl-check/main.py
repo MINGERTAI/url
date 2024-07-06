e9958f4ab5254243fef45b6e1381ed107760a1e0
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-06 10:47:32 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -40,11 +40,11 @@ if menu == 'tvbox':
                     if (addtv + spare + nsfw).find(j) > -1:
                         continue
                     # 过滤重复Key的电影网站
-                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
+                    # if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
                         # spare += '\r\n' + j + ','
-                        continue
+                        # continue
                     else:
-                        spare += '\r\n' + j + ','                
+                        # spare += '\r\n' + j + ','                
                     if tv['name'].find('*') > -1:
                         nsfw += '\r\n' + j + ','
             except Exception as ex:
