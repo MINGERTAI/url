fdc0e73210c976c1fd099577c5c0d89aed97f552
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-06 10:51:08 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -43,10 +43,11 @@ if menu == 'tvbox':
                     # if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
                         # spare += '\r\n' + j + ','
                         # continue
-                    else:
-                        spare += '\r\n' + j + ','                
+                    #else:
+                        #spare += '\r\n' + j + ','                
                     if tv['name'].find('*') > -1:
                         nsfw += '\r\n' + j + ','
+                        continue
             except Exception as ex:
                 LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
