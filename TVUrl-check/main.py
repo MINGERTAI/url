5e8ca9d4af1a19e57767c5326a4ab9470491a55c
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-06 09:47:26 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -25,35 +25,7 @@ if menu == 'tvbox':
         else:
             tvbox = LocalFile.read_LocalFile('./c.json')  #.replace('\r', '').replace('\n\n', '\n')
 
-        addtv = ''
-        nsfw = ''
-        spare = ''
-        #tvbox = tvbox.replace('//{', '\n{')
-        for j in tvbox.split('\n'):
-            try:
-                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 == -1:
-                    j = j.strip(',')
-                    if len(j.split('}')) > len(j.split('{')):
-                        j = j.strip(',')[:-1].strip(',')
-                    tv = json.loads(j)
-                    # 过滤重复的电影网站
-                    if (addtv + spare + nsfw).find(j) > -1:
-                        continue
-                    # 过滤重复Key的电影网站
-                    if (addtv + nsfw).find('"key":"' + tv['key'] + '"') > -1:
-                        spare += '\r\n' + j + ','
-                        continue
-                    else:
-                        spare += '\r\n' + j + ','                
-                    if tv['name'].find('*') > -1:
-                        nsfw += '\r\n' + j + ','
-                    elif j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
-                        addtv += '\r\n' + j + ','
-            except Exception as ex:
-                LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
-        
-        content = addtv + '\r\n' + nsfw + '\r\n' + spare
-        LocalFile.write_LocalFile('./out/123.txt', content)
+        LocalFile.write_LocalFile('./out/123.txt', tvbox)
         print('Line-96:./out/123.txt已更新。')
 
     except Exception as ex:
