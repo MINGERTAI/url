9e58920bb80a14a7472a84b107e2ebbca78f129d
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-06 10:19:08 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -52,7 +52,7 @@ if menu == 'tvbox':
             except Exception as ex:
                 LocalFile.write_LogFile('Main-Line-93-Exception:' + str(ex) + '\ntvsite:' + j)
         
-        content = addtv + '\r\n' + nsfw + '\r\n' + spare
+        #content = addtv + '\r\n' + nsfw + '\r\n' + spare
         LocalFile.write_LocalFile('./out/11.txt', content)
         print('Line-96:./out/11.txt已更新。')
 
