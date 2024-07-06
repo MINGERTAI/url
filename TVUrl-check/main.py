dc33ce53e58229eea7c8f44f17198ff97bdcdaaa
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-06 13:04:03 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -24,21 +24,23 @@ if menu == 'tvbox':
         else:
             tvbox = LocalFile.read_LocalFile('./out/123.json') #.replace('\r', '').replace('\n\n', '\n')
 
-        addtv = ''
-        nsfw = ''
+        #addtv = ''
+        #nsfw = ''
         spare = ''
         for j in tvbox.split('\n'):
             try:
                 if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 == -1:
                     # 过滤重复的电影网站
-                    if (addtv + spare + nsfw).find(j) > -1:
+                    if (spare).find(j) > -1:
+                    #if (addtv + spare + nsfw).find(j) > -1:
                         continue
                     spare += '\r\n' + j + ','
                     
             except Exception as ex:
                 LocalFile.write_LogFile(str(ex) + j)
         
-        content = addtv + '\r\n' + nsfw + '\r\n' + spare
+        content =  spare
+        #content = addtv + '\r\n' + nsfw + '\r\n' + spare
         LocalFile.write_LocalFile('./out/11.txt', content)
         print('读取并删除:./out/11.txt已更新。')
 
