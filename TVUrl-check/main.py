b1afdebee8ddb4a549b623300bce1cc9b5eaf4a4
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-07 21:03:13 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -4,35 +4,37 @@ from cls import LocalFile  # 假设这是你自定义的本地文件操作类
 
 headers = {'User-Agent': 'okhttp/3.15'}
 
-def download_file():
-    """
-    从指定 URL 下载文件并保存到本地路径。
-    """
+def remove_line(content):
+
     try:
         # 发送 HTTP GET 请求
-        url = "http://肥猫.com"
+        url = "https://raw.githubusercontent.com/qist/tvbox/master/dianshi.json"
         response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            # 转换响应的 JSON 数据为字符串
-            tvbox = json.dumps(response.json())
-            spare = ''
-            for j in tvbox.split('\n'):
-                try:
-                    if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
-                        # 过滤重复的电影网站
-                        if spare.find(j) > -1:
-                            continue
-                        spare += '\r\n' + j
-                except Exception as ex:
-                    LocalFile.write_LogFile(f"解析行时出错: {str(ex)} 行内容: {j}")
+        
+        # 检查请求是否成功
+        response.raise_for_status()
+
+        # 解析 JSON 内容
+        tvbox = response.text
+        spare = ''
+        
+        # 分行处理 JSON 内容
+        for j in tvbox.split('\n'):
+            try:
+                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1:
+                    # 过滤重复的电影网站
+                    if spare.find(j) > -1:
+                        continue
+                    spare += '\r\n' + j
+            except Exception as ex:
+                LocalFile.write_LogFile(f"解析行时出错: {str(ex)} 行内容: {j}")
    
-            content = spare
-            LocalFile.write_LocalFile('./out/10.txt', content)
-            print('读取并删除: ./out/10.txt 已更新。')
+        content = spare
+        content = remove_url_key(content)
+        LocalFile.write_LocalFile('./out/new.txt', content)
+        print('读取并删除:./out/new.txt已更新。')
     
     except Exception as ex:
         LocalFile.write_LogFile(f"下载或处理文件时出错: {str(ex)}")
 
-# 脚本的主逻辑
-if __name__ == "__main__":
-    download_file()
+    return content
