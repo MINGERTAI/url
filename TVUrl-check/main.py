7bb7d0fd538b6f709586fa1f28a4ddafd731f1ef
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 16:24:49 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -1,5 +1,8 @@
 import json
-import pprint
-from cls import LocalFile
-tvbox = LocalFile.read_LocalFile('./fatcat.json')
-pprint.pprint(tvbox)
+with open("fatcat.json","rb") as f:
+    dic = json.load(f)
+    sites = dic["sites"]
+    sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
+    sites.remove(sites[-1])  ## 删除最后一行
+with open("./out/new_test.json","wb") as f:
+    f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
