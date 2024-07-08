727d159c2fcc33516a246fcf0f0c3afedfc399d9
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 18:12:14 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -2,9 +2,19 @@ import json
 import pprint
 
 data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
+    "key": "drpy_js_豆瓣",
+    "name": "🐼┃公众号┃肥猫宝贝",
+    "type": 3,
+    "api": "csp_Douban",
+    "searchable": 0
+},
+{
+    "key": "豆瓣",
+    "name": "🐼┃豆瓣┃预告",
+    "type": 3,
+    "api": "csp_YGP",
+    "playerType": 2,
+    "searchable": 0
 }
 
 pprint.pprint(data)
