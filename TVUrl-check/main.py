5e14710cef6a2f7a3e379514dfd44929dbc0fc72
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 17:47:43 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -1,17 +1,6 @@
 import json
-with open("fatcat.json","rb") as f:
-    dic = json.load(f)
-    sites = dic["sites"]
-    sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
-    sites.remove(sites[-1])  ## 删除最后一行
-    
-local_content = json.dumps(dic,ensure_ascii=False)
-with open('./out/new_test.json', 'w', encoding='utf-8') as f:
-    for line in local_content.split('\n'):  # 将内容按行分割
-        if line.strip():  # 如果该行非空（移除空白字符后有内容）
-            if '},' in line:
-                line.strip('\r\n')
-                f.write(line + '\n')  # 将非空行写入到文件中，记得在最后加上 '\n' 以保持原有的行分割
-#with open("./out/new_test.json","wb") as f:
-    #f.write(json.dumps(dic,ensure_ascii=False).encode("utf-8"))
+from cls import LocalFile
+tvbox = LocalFile.read_LocalFile('fatcat.json')
+with open("./out/new_test.json","wb") as f:
+    f.write(json.dumps(tvbox,ensure_ascii=False).encode("utf-8"))
     # f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
