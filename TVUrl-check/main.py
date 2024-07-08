6fc433634205c0194597ba4a97c58bcb9f4b6105
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 16:25:49 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -5,4 +5,5 @@ with open("fatcat.json","rb") as f:
     sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
     sites.remove(sites[-1])  ## 删除最后一行
 with open("./out/new_test.json","wb") as f:
-    f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
+    f.write(json.dumps(dic,ensure_ascii=False).encode("utf-8"))
+    # f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
