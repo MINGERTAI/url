a0d25d5f29c32517bd4374b0ae9919572bbba07c
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 16:48:33 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -6,7 +6,7 @@ with open("fatcat.json","rb") as f:
     sites.remove(sites[-1])  ## 删除最后一行
 
 with open('./out/new_test.json', 'w', encoding='utf-8') as f:
-    for line in json.dumps(dic.split('\n')):  # 将内容按行分割
+    for line in dic.split('\n'):  # 将内容按行分割
         if line.strip():  # 如果该行非空（移除空白字符后有内容）
             f.write(line + '\n')  # 将非空行写入到文件中，记得在最后加上 '\n' 以保持原有的行分割
 #with open("./out/new_test.json","wb") as f:
