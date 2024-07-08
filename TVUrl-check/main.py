69c206d23169121c8676fbe2e47efe39fe5d2f9a
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 18:07:47 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -2,11 +2,9 @@ import json
 import pprint
 
 data = {
-            "key": "米搜",
-            "name": "🌖┃米搜┃网盘",
-            "type": 3,
-            "api": "csp_MiSou",
-            "ext": "http://127.0.0.1:9978/file/fatcat/kk.txt"
-        },
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
 
 pprint.pprint(data)
