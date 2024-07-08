343f035cd4c3a68e2211c1006b97c440620e2626
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 16:19:57 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -2,6 +2,6 @@ import json
 import pprint
 from cls import LocalFile
 tvbox = LocalFile.read_LocalFile('./fatcat.json')
-data = pprint(tvbox)
+data = pprint.pprint(tvbox)
 with open("./out/new_test.json","wb") as f:
     f.write(data)
