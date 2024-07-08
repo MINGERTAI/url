74cd8a7c9fe66e4efcff8bb8bd158e7b6c8044d5
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 16:51:27 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -4,9 +4,10 @@ with open("fatcat.json","rb") as f:
     sites = dic["sites"]
     sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
     sites.remove(sites[-1])  ## 删除最后一行
-
+    
+local_content = json.dumps(dic,ensure_ascii=False)
 with open('./out/new_test.json', 'w', encoding='utf-8') as f:
-    for line in dic.split('\n'):  # 将内容按行分割
+    for line in local_content.split('\n'):  # 将内容按行分割
         if line.strip():  # 如果该行非空（移除空白字符后有内容）
             f.write(line + '\n')  # 将非空行写入到文件中，记得在最后加上 '\n' 以保持原有的行分割
 #with open("./out/new_test.json","wb") as f:
