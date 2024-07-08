5c7ae9439905750caec014a17178844aa18ccbe1
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 15:07:27 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -1,44 +1,8 @@
-#!/usr/bin/env python3
-
-import datetime
-from gettext import find
 import json
-import os
-import requests
-import sys
-from cls import LocalFile
-from cls import NetFile
-
-# 获取传递的参数
-try:
-    #0表示文件名，1后面都是参数 0.py, 1, 2, 3
-    menu = sys.argv[1:][0]
-    if(len(sys.argv[1:]) > 1):
-        cid = sys.argv[1:][1]
-except:
-    menu = 'check'
-print('menu: ' + menu)
-
-# 下载Node.json中的所有Url订阅链接将其合并，生成本地vpei-new.txt，同步至Github后改名为vpei.txt文件
-if(menu == 'check'):
-    try:
-        if(os.path.exists('./fatcat.json')):
-            tvbox = LocalFile.read_LocalFile('./fatcat.json').replace('\r','').replace('\n\n','\n')
-        spare = ''
-        for j in tvbox.strip():
-            try:
-                if j != '' and j.find('"key":') > -1 and j.find('"name":') > -1 and j.find('"type":') > -1 == -1:
-                    # 过滤重复的电影网站
-                    if (spare).find(j) > -1:
-                        continue
-                    spare += '\r\n' + j
-                    
-            except Exception as ex:
-                LocalFile.write_LogFile(str(ex) + j)
-        
-        content = spare
-        LocalFile.write_LocalFile('./out/1102.txt', content)
-        print('读取并删除&写入到:./out/1102.txt已更新。')
-
-    except Exception as ex:
-        LocalFile.write_LogFile(str(ex))
+with open("fatcat.json","rb") as f:
+    dic = json.load(f)
+    sites = dic["sites"]
+    sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
+    sites.remove(sites[-1])  ## 删除最后一行
+with open("./out/new_test.json","wb") as f:
+    f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
