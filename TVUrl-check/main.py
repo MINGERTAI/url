2e6fe7cf81c120dc40479422577d52fa25849b24
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 17:13:54 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -8,7 +8,7 @@ with open("fatcat.json","rb") as f:
 local_content = json.dumps(dic,ensure_ascii=False)
 fuhao = json.dumps( ',' )
 with open('./out/new_test.json', 'w', encoding='utf-8') as f:
-    for line in local_content.split(','):  # 将内容按行分割
+    for line in local_content.split('\n'):  # 将内容按行分割
         if line.strip():  # 如果该行非空（移除空白字符后有内容）
             f.write(line + '\n')  # 将非空行写入到文件中，记得在最后加上 '\n' 以保持原有的行分割
 #with open("./out/new_test.json","wb") as f:
