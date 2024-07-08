02682cc91815ff9c1b24667d194adf5468d69d44
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-08 15:58:31 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -1,8 +1,5 @@
 import json
 import pprint
 with open("fatcat.json","rb") as f:
-    dic = json.load(f)
-    sites = dic["sites"]
-    sites.append({'key': 'test', 'name': 'test', 'type': 3, 'api': 'csp_Douban', 'searchable': 0})  ## 新增
 with open("./out/new_test.json","wb") as f:
-    f.write(json.dumps(dic,ensure_ascii=False,indent=4).encode("utf-8"))
+    f.write(json.dumps(pprint(data)).encode("utf-8"))
