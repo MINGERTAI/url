8969cfbb5b09e81958b5c67b8b6dec31afcd799e
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-10 17:51:43 +0800

    Update main.py

--- TVUrl-check/main.py
+++ TVUrl-check/main.py
@@ -1,18 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-def download_json(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        json_data = soup.find_all('script', type='application/json')[0].text.strip()
-        with open('./out/tvbox.json', 'w') as file:
-            file.write(json_data)
-        print("JSON文件已下载到当前目录下的'tvbox/tvbox.json'")
-    else:
-        print(f"请求失败，状态码：{response.status_code}")
+url = 'http://tvbox.王二小放牛娃.xyz'
+response = requests.get(url)
 
-# 网页URL
-url = "http://tvbox.王二小放牛娃.xyz"
-
-download_json(url)
+if response.status_code == 200:
+    json_data = response.json()
+    print(json_data)
+else:
+    print("Failed to get JSON data.")
