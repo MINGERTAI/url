0c8c8d34ed68a5fac1b266b813445592083b844c
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-13 13:38:22 +0800

    Update main.py

--- fan/main.py
+++ fan/main.py
@@ -27,8 +27,8 @@ def get_dianshi():
 
     try:
         # 删除原有的 ./out/new.txt 文件（如果存在）
-        if os.path.exists('./out/new.txt'):
-            newtxt = ('./out/new.txt')
+        if os.path.exists('out/new.txt'):
+            newtxt = ('out/new.txt')
             os.remove('newtxt')
         else:
             print('已删除原有的 ./out/new.txt 文件。')
