2958bd0864985090e420cf11db64acac5ff6830e
ne7359 <158133792+ne7359@users.noreply.github.com>
2024-07-13 13:50:35 +0800

    Update main.py

--- fan/main.py
+++ fan/main.py
@@ -58,7 +58,7 @@ def get_dianshi():
         content = spare
         content = remove_url_key(content)
         LocalFile.write_LocalFile('./out/new.txt', content)
-        print('读取并删除:./out/new.txt已更新。')
+        print('写入最新的:./out/new.txt已更新。')
     
     except Exception as ex:
         LocalFile.write_LogFile(f"下载或处理文件时出错: {str(ex)}")
